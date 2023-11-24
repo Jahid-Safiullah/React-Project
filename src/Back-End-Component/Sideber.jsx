446f868b9ae4cd3de82cffe54756e2a7bc522c59
jahid jahid <jahidsafiulla16@gmail.com>
2023-11-24 23:48:27 +0600

    update

--- src/Back-End-Component/Sideber.jsx
+++ src/Back-End-Component/Sideber.jsx
@@ -56,14 +56,6 @@ const Sidebar = () => {
 export default Sidebar
 
 
-{/* <div className="bakendsideber">
-      <Nav defaultActiveKey="/home" className=" flex-column">
-      <NavLink className={"itemdesign"} to="/home">Active</NavLink>
-      <NavLink className={"itemdesign"} to="/BusDashbord">Bus Dashbord</NavLink>
-      <NavLink className={"itemdesign"} to="LaunchDashbord">Launch Dashbord</NavLink>
-      <NavLink className={"itemdesign"} to="/TrainDashbord" >Train Dashbord</NavLink>
-    </Nav>
-      </div> */}
 
 
 
