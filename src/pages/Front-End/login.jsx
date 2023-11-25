738630d4ba0b201b0b0f3253d46b3fe79b734cf6
Jahid200 <jahidsafiulla16@gmail.com>
2023-11-25 13:08:31 +0600

    update data to database

--- src/pages/Front-End/login.jsx
+++ src/pages/Front-End/login.jsx
@@ -1,11 +1,6 @@
 import { useState } from 'react';
 import TopHeder from '../../Front-End-Component/TopHeder';
 import Navber from '../../Front-End-Component/Navbar/Navber';
-import Button from 'react-bootstrap/Button';
-import Col from 'react-bootstrap/Col';
-import Form from 'react-bootstrap/Form';
-import InputGroup from 'react-bootstrap/InputGroup';
-import Row from 'react-bootstrap/Row';
 import { useNavigate } from 'react-router-dom';
 import Footer from '../../Front-End-Component/footer/footer';
 
@@ -14,16 +9,36 @@ import Footer from '../../Front-End-Component/footer/footer';
 
 
 
-function LoginForm() {
-  <div>
-    
-  </div>
+
+async function signUp() {
+  let item = { name, email, password };
+  console.warn(item);
+
+  try {
+    let result = await fetch("http://localhost:8000/api/register", {
+      method: 'POST',
+      body: JSON.stringify(item),
+      headers: {
+        "Content-Type": "application/json",  // Corrected header name
+        "Accept": "application/json"
+      }
+    });
+
+    result = await result.json();
+    console.warn("result", result);
+  } catch (error) {
+    console.error("Error during sign-up:", error);
+  }
+}
+
+
+
+
+  
   const navigate = useNavigate();
   const [validated, setValidated] = useState(false);
-  
   function click() {navigate("/Sidebar")
-    window.location.reload(true);
-  
+    window.location.reload(true); 
   }
   
   const handleSubmit = (event) => {
@@ -32,90 +47,34 @@ function LoginForm() {
       event.preventDefault();
       event.stopPropagation();
     }
-
     setValidated(true);
     
   };
+
+
+
 
   return (
     <div>
       <div>
       <TopHeder />
-                <Navber />
+      <Navber />
       </div>
-    <Form style={{justifyContent:"center",height:"1000px",
-    background: "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(63,136,1,0.9444152661064426) 30%, rgba(0,212,255,1) 100%)"}} className='tex-aline-center' noValidate validated={validated} onSubmit={handleSubmit}>
-      
-      <Row className="" style={{marginLeft:"300px",marginRight:"300px"}}>
-        <Form.Group as={Col} md="4" controlId="validationCustom01">
-          <Form.Label>First name</Form.Label>
-          <Form.Control
-            required
-            type="text"
-            placeholder="First name"
-            defaultValue="Mark"
-          />
-          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-        </Form.Group>
-        <Form.Group as={Col} md="4" controlId="validationCustom02">
-          <Form.Label>Last name</Form.Label>
-          <Form.Control
-            required
-            type="text"
-            placeholder="Last name"
-            defaultValue="Otto"
-          />
-          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-        </Form.Group>
-        <Form.Group as={Col} md="4" controlId="validationCustomUsername">
-          <Form.Label>Username</Form.Label>
-          <InputGroup hasValidation>
-            <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
-            <Form.Control
-              type="text"
-              placeholder="Username"
-              aria-describedby="inputGroupPrepend"
-              required
-            />
-            <Form.Control.Feedback type="invalid">
-              Please choose a username.
-            </Form.Control.Feedback>
-          </InputGroup>
-        </Form.Group>
-      </Row>
-      <Row className="mb-3" style={{marginLeft:"300px",marginRight:"300px"}}>
-        <Form.Group as={Col} md="6" controlId="validationCustom03">
-          <Form.Label>City</Form.Label>
-          <Form.Control type="text" placeholder="City" required />
-          <Form.Control.Feedback type="invalid">
-            Please provide a valid city.
-          </Form.Control.Feedback>
-        </Form.Group>
-        <Form.Group as={Col} md="3" controlId="validationCustom04">
-          <Form.Label>State</Form.Label>
-          <Form.Control type="text" placeholder="State" required />
-          <Form.Control.Feedback type="invalid">
-            Please provide a valid state.
-          </Form.Control.Feedback>
-        </Form.Group>
-        <Form.Group as={Col} md="3" controlId="validationCustom05">
-          <Form.Label>Zip</Form.Label>
-          <Form.Control type="text" placeholder="Zip" required />
-          <Form.Control.Feedback type="invalid">
-            Please provide a valid zip.
-          </Form.Control.Feedback>
-        </Form.Group>
-      </Row>
-      <Form.Group className="mb-3"style={{marginLeft:"315px",marginRight:"300px"}}>
-        <Form.Check
-          required
-          label="Agree to terms and conditions"
-          feedback="You must agree before submitting."
-          feedbackType="invalid"
-        />
-      </Form.Group>
-      <Button type="submit"onClick={click}>Submit form</Button>
-    </Form>
+      <div class="col-sm-6 offset-sm-3" >
+        <div class="input-group input-group-sm mb-3">
+          <span class="input-group-text" id="inputGroup-sizing-sm">Name:</span>
+          <input value={name} onChange={(e)=>setName(e.target.value)} type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"/>
+        </div>
+        <div class="input-group input-group-sm mb-3">
+          <span class="input-group-text" id="inputGroup-sizing-sm">Email:</span>
+          <input value={email} onChange={(e)=>setEmail(e.target.value)} type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"/>
+        </div>
+        <div class="input-group input-group-sm mb-3">
+          <span class="input-group-text" id="inputGroup-sizing-sm">Password:</span>
+          <input value={password} onChange={(e)=>setPassword(e.target.value)} type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"/>
+        </div>
+        <button onClick={signUp} className="btn btn-primary">Click me</button>
+      </div>
     <div><Footer /></div>
     </div>
   );
